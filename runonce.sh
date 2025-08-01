68f0b1a4dd14d15a2d04457023420b24e968b523
berkay <berkaygursoy@gmail.com>
2025-08-01 23:06:32 +0300

    copy files

--- runonce.sh
+++ runonce.sh
@@ -9,15 +9,15 @@ fi
 # Copy files 
 if test -d /home/admin/SoraxRasperry; then
     echo "Writing new source files"
-    # cp /home/admin/SoraxRasperry/listener.py /home/admin/listener.py
+    cp /home/admin/SoraxRasperry/listener.py /home/admin/listener.py
     # cp /home/admin/SoraxRasperry/modemConnect.py /home/admin/modemConnect.py
     # cp /home/admin/SoraxRasperry/modemCycle.py /home/admin/modemCycle.py
     # cp /home/admin/SoraxRasperry/boot_sim_configure.py /home/admin/boot_sim_configure.py
-    # cp /home/admin/SoraxRasperry/fonks.py /home/admin/fonks.py
+    cp /home/admin/SoraxRasperry/fonks.py /home/admin/fonks.py
 
-    # cp /home/admin/SoraxRasperry/create_log.sh /home/admin/create_log.sh
+    cp /home/admin/SoraxRasperry/create_log.sh /home/admin/create_log.sh
     cp /home/admin/SoraxRasperry/upload_log.sh /home/admin/upload_log.sh
-    # cp /home/admin/SoraxRasperry/fetch_source.sh /home/admin/fetch_source.sh
+    cp /home/admin/SoraxRasperry/fetch_source.sh /home/admin/fetch_source.sh
     cp /home/admin/SoraxRasperry/runonce.sh /home/admin/runonce.sh
 
     chmod +x /home/admin/runonce.sh
