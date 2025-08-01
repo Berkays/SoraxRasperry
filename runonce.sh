498f314b3abe7777006e85fa9185ad89bfb9d25c
berkay <berkaygursoy@gmail.com>
2025-08-01 22:39:46 +0300

    cp runonce

--- runonce.sh
+++ runonce.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# sudo /home/admin/listener_env/bin/python3 /home/admin/listener.py
 str=0
 if [[ "$(< /home/admin/update.txt)" == "$str" ]]; then
     # No runonce update
@@ -9,7 +9,6 @@ fi
 # Copy files 
 if test -d /home/admin/SoraxRasperry; then
     echo "Writing new source files"
-    cp /home/admin/SoraxRasperry/test.py /home/admin/test.py
     # cp /home/admin/SoraxRasperry/listener.py /home/admin/listener.py
     # cp /home/admin/SoraxRasperry/modemConnect.py /home/admin/modemConnect.py
     # cp /home/admin/SoraxRasperry/modemCycle.py /home/admin/modemCycle.py
@@ -19,7 +18,7 @@ if test -d /home/admin/SoraxRasperry; then
     # cp /home/admin/SoraxRasperry/create_log.sh /home/admin/create_log.sh
     # cp /home/admin/SoraxRasperry/upload_log.sh /home/admin/upload_log.sh
     # cp /home/admin/SoraxRasperry/fetch_source.sh /home/admin/fetch_source.sh
-    # cp /home/admin/SoraxRasperry/runonce.sh /home/admin/runonce.sh
+    cp /home/admin/SoraxRasperry/runonce.sh /home/admin/runonce.sh
 fi
 
 echo "0" > /home/admin/update.txt
