35b4809b333034254e60a9373a3c491ec0515d17
berkay <berkaygursoy@gmail.com>
2025-08-01 22:49:55 +0300

    add run log

--- upload_log.sh
+++ upload_log.sh
@@ -9,3 +9,12 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/octet-stream' \
   --data-binary '@/home/admin/log.tar.gz'
+
+
+tar cvzf /home/admin/runlog.tar.gz -C /run/log/ .
+
+curl -X 'POST' \
+  "https://filebin.net/sorax_log_bin/runlog_\"$timestamp\".tar.gz" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/octet-stream' \
+  --data-binary '@/home/admin/runlog.tar.gz'
